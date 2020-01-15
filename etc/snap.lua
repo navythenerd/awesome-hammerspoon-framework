da45217f7cdb9b6aba43a6786f767c6cf7e2e613
ckaufmann <cedrik.kaufmann@gmail.com>
2020-01-15 22:09:13 +0100

    Update snap mod. Snap now supports highly customizable profiles, which can be set up in config file.

--- etc/snap.lua
+++ etc/snap.lua
@@ -1,15 +1,53 @@
 local settings = {}
 
-settings.keymap = {
-  {hyper = "hyper", key = "RETURN", fn = ahf.mod.snap.fullscreen},
-  {hyper = "hyper", key = "LEFT", fn = ahf.mod.snap.leftHalf},
-  {hyper = "hyper", key = "RIGHT", fn = ahf.mod.snap.rightHalf},
-  {hyper = "hyper", key = "UP", fn = ahf.mod.snap.topHalf},
-  {hyper = "hyper", key = "DOWN", fn = ahf.mod.snap.bottomHalf},
-  {hyper = "hyper", key = "u", fn = ahf.mod.snap.leftTopQuarter},
-  {hyper = "hyper", key = "i", fn = ahf.mod.snap.rightTopQuarter},
-  {hyper = "hyper", key = "j", fn = ahf.mod.snap.leftBottomQuarter},
-  {hyper = "hyper", key = "k", fn = ahf.mod.snap.rightBottomQuarter}
+settings.use = "ultrawide"
+
+settings.profiles = {
+  ultrawide = {
+    {hyper = "hyper", key = "RETURN", xSplit = 1, ySplit = 1, xPos = 1, yPos = 1},
+
+    {hyper = "hyper", key = "1", xSplit = 3, ySplit = 1, xPos = 1, yPos = 1},
+    {hyper = "hyper", key = "2", xSplit = 3, ySplit = 1, xPos = 2, yPos = 1},
+    {hyper = "hyper", key = "3", xSplit = 3, ySplit = 1, xPos = 3, yPos = 1},
+
+    {hyper = "hyper", key = "4", xSplit = 4, ySplit = 1, xPos = 1, yPos = 1},
+    {hyper = "hyper", key = "5", xSplit = 4, ySplit = 1, xPos = 2, yPos = 1},
+    {hyper = "hyper", key = "6", xSplit = 4, ySplit = 1, xPos = 3, yPos = 1},
+    {hyper = "hyper", key = "7", xSplit = 4, ySplit = 1, xPos = 4, yPos = 1},
+
+    {hyper = "hyper", key = "q", xSplit = 4, ySplit = 2, xPos = 1, yPos = 1},
+    {hyper = "hyper", key = "w", xSplit = 4, ySplit = 2, xPos = 2, yPos = 1},
+    {hyper = "hyper", key = "e", xSplit = 4, ySplit = 2, xPos = 3, yPos = 1},
+    {hyper = "hyper", key = "r", xSplit = 4, ySplit = 2, xPos = 4, yPos = 1},
+
+    {hyper = "hyper", key = "t", xSplit = 4, ySplit = 2, xPos = 1, yPos = 2},
+    {hyper = "hyper", key = "z", xSplit = 4, ySplit = 2, xPos = 2, yPos = 2},
+    {hyper = "hyper", key = "u", xSplit = 4, ySplit = 2, xPos = 3, yPos = 2},
+    {hyper = "hyper", key = "i", xSplit = 4, ySplit = 2, xPos = 4, yPos = 2},
+
+    {hyper = "hyper", key = "a", xSplit = 3, ySplit = 2, xPos = 1, yPos = 1},
+    {hyper = "hyper", key = "s", xSplit = 3, ySplit = 2, xPos = 2, yPos = 1},
+    {hyper = "hyper", key = "d", xSplit = 3, ySplit = 2, xPos = 3, yPos = 1},
+
+    {hyper = "hyper", key = "f", xSplit = 3, ySplit = 2, xPos = 1, yPos = 2},
+    {hyper = "hyper", key = "g", xSplit = 3, ySplit = 2, xPos = 2, yPos = 2},
+    {hyper = "hyper", key = "h", xSplit = 3, ySplit = 2, xPos = 3, yPos = 2},
+
+    {hyper = "cmd_alt_shift", key = "q", xSplit = 4, ySplit = 3, xPos = 1, yPos = 1},
+    {hyper = "cmd_alt_shift", key = "w", xSplit = 4, ySplit = 3, xPos = 2, yPos = 1},
+    {hyper = "cmd_alt_shift", key = "e", xSplit = 4, ySplit = 3, xPos = 3, yPos = 1},
+    {hyper = "cmd_alt_shift", key = "r", xSplit = 4, ySplit = 3, xPos = 4, yPos = 1},
+
+    {hyper = "cmd_alt_shift", key = "a", xSplit = 4, ySplit = 3, xPos = 1, yPos = 2},
+    {hyper = "cmd_alt_shift", key = "s", xSplit = 4, ySplit = 3, xPos = 2, yPos = 2},
+    {hyper = "cmd_alt_shift", key = "d", xSplit = 4, ySplit = 3, xPos = 3, yPos = 2},
+    {hyper = "cmd_alt_shift", key = "f", xSplit = 4, ySplit = 3, xPos = 4, yPos = 2},
+
+    {hyper = "cmd_alt_shift", key = "y", xSplit = 4, ySplit = 3, xPos = 1, yPos = 3},
+    {hyper = "cmd_alt_shift", key = "x", xSplit = 4, ySplit = 3, xPos = 2, yPos = 3},
+    {hyper = "cmd_alt_shift", key = "c", xSplit = 4, ySplit = 3, xPos = 3, yPos = 3},
+    {hyper = "cmd_alt_shift", key = "v", xSplit = 4, ySplit = 3, xPos = 4, yPos = 3},
+  },
 }
 
 return settings
--- mod/snap.lua
+++ mod/snap.lua
@@ -2,156 +2,34 @@ local mod = {}
 
 mod.namespace = 'snap'
 
-function mod.fullscreen()
+local function divide(xMax, yMax, x, y)
   if hs.window.focusedWindow() then
     local win = hs.window.focusedWindow()
     local f = win:frame()
     local screen = win:screen()
     local max = screen:frame()
 
-    f.x = max.x
-    f.y = max.y
-    f.w = max.w
-    f.h = max.h
+    f.x = max.x + (max.w / xMax) * (x - 1)
+    f.y = max.y + (max.h / yMax) * (y - 1)
+    f.w = max.w / xMax
+    f.h = max.h / yMax
+
     win:setFrame(f)
   else
     hs.alert.show("No active window")
   end
 end
 
-function mod.topHalf()
-  if hs.window.focusedWindow() then
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
+function mod.init()
+  profile = mod.context.config.profiles[mod.context.config.use]
 
-    f.x = max.x
-    f.y = max.y
-    f.w = max.w
-    f.h = max.h / 2
-    win:setFrame(f)
-  else
-    hs.alert.show("No active window")
-  end
-end
-
-function mod.bottomHalf()
-if hs.window.focusedWindow() then
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    f.x = max.x
-    f.y = max.y + (max.h / 2)
-    f.w = max.w
-    f.h = max.h / 2
-    win:setFrame(f)
-  else
-    hs.alert.show("No active window")
-  end
-end
-
-function mod.leftHalf()
-  if hs.window.focusedWindow() then
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    f.x = max.x
-    f.y = max.y
-    f.w = max.w / 2
-    f.h = max.h
-    win:setFrame(f)
-  else
-    hs.alert.show("No active window")
-  end
-end
-
-function mod.rightHalf()
-  if hs.window.focusedWindow() then
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    f.x = max.x + (max.w / 2)
-    f.y = max.y
-    f.w = max.w / 2
-    f.h = max.h
-    win:setFrame(f)
-  else
-    hs.alert.show("No active window")
-  end
-end
-
-function mod.leftTopQuarter()
-  if hs.window.focusedWindow() then
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    f.x = max.x
-    f.y = max.y
-    f.w = max.w / 2
-    f.h = max.h / 2
-    win:setFrame(f)
-  else
-    hs.alert.show("No active window")
-  end
-end
-
-function mod.rightTopQuarter()
-  if hs.window.focusedWindow() then
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    f.x = max.x + (max.w / 2)
-    f.y = max.y
-    f.w = max.w / 2
-    f.h = max.h / 2
-    win:setFrame(f)
-  else
-    hs.alert.show("No active window")
-  end
-end
-
-function mod.leftBottomQuarter()
-  if hs.window.focusedWindow() then
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    f.x = max.x
-    f.y = max.y + (max.h / 2)
-    f.w = max.w / 2
-    f.h = max.h / 2
-    win:setFrame(f)
-  else
-    hs.alert.show("No active window")
-  end
-end
-
-function mod.rightBottomQuarter()
-  if hs.window.focusedWindow() then
-    local win = hs.window.focusedWindow()
-    local f = win:frame()
-    local screen = win:screen()
-    local max = screen:frame()
-
-    f.x = max.x + (max.w / 2)
-    f.y = max.y + (max.h / 2)
-    f.w = max.w / 2
-    f.h = max.h / 2
-    win:setFrame(f)
-  else
-    hs.alert.show("No active window")
+  for n, entry in ipairs(profile) do
+    if (entry.hyper ~= nil and entry.key ~= nil and entry.xSplit ~= nil and entry.ySplit ~= nil and entry.xPos ~= nil and entry.yPos ~= nil and type(entry.hyper) == 'string' and type(entry.key) == 'string' and type(entry.xSplit) == 'number' and type(entry.ySplit) == 'number' and type(entry.xPos) == 'number' and type(entry.yPos) == 'number') then
+      wrapperFn = function() divide(entry.xSplit, entry.ySplit, entry.xPos, entry.yPos) end
+      ahf.bindHotkey(entry.hyper, entry.key, wrapperFn)
+    else
+      mod.context.logger.e("Error in snap profile: " .. mod.context.config.use)
+    end
   end
 end
 
